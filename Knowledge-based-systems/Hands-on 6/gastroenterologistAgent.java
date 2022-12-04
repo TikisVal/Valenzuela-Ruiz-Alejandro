62f351fc8af649f9933c84dc1ac28c4be3227f73
TikisVal <94882042+TikisVal@users.noreply.github.com>
2022-12-04 12:28:40 -0600

    Update gastroenterologistAgent.java

--- Knowledge-based-systems/Hands-on 6/gastroenterologistAgent.java
+++ Knowledge-based-systems/Hands-on 6/gastroenterologistAgent.java
@@ -1,1 +1,36 @@
-s
+package examples.messaging;
+
+import jade.core.*;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+ 
+public class gastroenterologistAgent extends Agent {
+   
+    protected void setup() {
+        addBehaviour(new gastroenterologistBehaviour());
+    }
+
+   private class gastroenterologistBehaviour extends SimpleBehaviour {
+            private boolean fin = false;
+       
+            public void action() {
+                System.out.println(" Preparing to receive");
+ 
+            //Obtiene el primer mensaje de la cola de mensajes
+                ACLMessage mensaje = receive();
+ 
+                if (mensaje!= null) {
+                    System.out.println(getLocalName() + ": you just receive the next message: ");
+                    System.out.println(mensaje.toString());
+                    clips.load("load-gastroenterologist-KB.clp");
+                    System.out.println("El diagnostico es: ");
+                    clips.run();
+                    fin = true;
+                }
+            }
+
+            public boolean done() {
+                return fin;
+            }
+    }
+}
