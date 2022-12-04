6adbe2faadbfbf86744f68c730687921d44d7c99
TikisVal <94882042+TikisVal@users.noreply.github.com>
2022-12-04 12:30:44 -0600

    Create pacientAgent2.java

---
+++ Knowledge-based-systems/Hands-on 6/pacientAgent2.java
@@ -0,0 +1,43 @@
+package examples.messaging;
+ 
+import jade.core.*;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
+ 
+public class pacientAgent2 extends Agent {
+
+    protected void setup() {
+        addBehaviour(new pacientBehaviour());
+    }
+
+   private class pacientBehaviour extends SimpleBehaviour {
+        boolean fin = false;
+      
+        public void action() {
+            System.out.println(getLocalName() +": Preparing to send a message to the medic");
+            AID id = new AID();
+            id.setLocalName("medic");
+ 
+        // Creación del objeto ACLMessage
+            ACLMessage mensaje = new ACLMessage(ACLMessage.INFORM);
+ 
+        //Rellenar los campos necesarios del mensaje
+            mensaje.setSender(getAID());
+            mensaje.setLanguage("English");
+            mensaje.addReceiver(id);
+            mensaje.setContent(clips.load("load-sintomas2.clp"));
+ 
+        //Envia el mensaje a los destinatarios
+            send(mensaje);
+ 
+            System.out.println(getLocalName() +": ... What are you up to");
+            System.out.println(mensaje.toString());
+            fin = true;
+        }
+ 
+        public boolean done()
+        {
+            return fin;
+        }
+    }
+}
