9733b063e38539e470e1022eca9d0d3290984f1b
TikisVal <94882042+TikisVal@users.noreply.github.com>
2022-12-03 20:43:02 -0600

    Update persons.java

--- Knowledge-based-systems/Challenge 2/persons.java
+++ Knowledge-based-systems/Challenge 2/persons.java
@@ -1,58 +1,41 @@
 import net.sf.clipsrules.jni.*;
 
-public class persons {
-    private static Environment clips;
+public class hands-on3 {
+    public class persons {
+        private static Environment clips;
 
-    protected static void setup() throws CLIPSException{
-        clips = new Environment();
-        clips.load("run-persons.clp");
-        clips.run();
+        protected static void setup() throws CLIPSException{
+            clips = new Environment();
+            clips.load("run-persons.clp");
+            clips.run();
+        }
     }
 
+    public class products {
+        private static Environment clips;
+
+        protected static void setup() throws CLIPSException{
+            clips = new Environment();
+            clips.load("run-products.clp");
+            clips.run();
+        }
+    }
+
+    public class market {
+        private static Environment clips;
+
+        protected static void setup() throws CLIPSException{
+            clips = new Environment();
+            clips.load("run-market.clp");
+            clips.run();
+        }
+    }
+    
     /**
-     * @param args
-     * @throws CLIPSException
-     */
+    * @param args
+    * @throws CLIPSException
+    */
     public static void main(String[] args) throws CLIPSException {
         setup();
     }
-}
-
-public class products {
-    private static Environment clips;
-
-    protected static void setup() throws CLIPSException{
-        clips = new Environment();
-        clips.load("run-products.clp");
-        clips.run();
-    }
-
-    /**
-     * @param args
-     * @throws CLIPSException
-     */
-    public static void main(String[] args) throws CLIPSException {
-        setup();
-    }
-}
-
-import net.sf.clipsrules.jni.*;
-
-public class market {
-    private static Environment clips;
-
-    protected static void setup() throws CLIPSException{
-        clips = new Environment();
-        clips.load("run-market.clp");
-        clips.run();
-    }
-
-    /**
-     * @param args
-     * @throws CLIPSException
-     */
-    public static void main(String[] args) throws CLIPSException {
-        setup();
-    }
-}
-
+}   
